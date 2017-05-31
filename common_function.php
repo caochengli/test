117f78d3976cc50988e920ecb65cab45994cdfa3
ccl <caocl@ssgchain.com>
2017-05-31 11:49:28 +0800

    新增常用基本方法

--- common_function.php
+++ common_function.php
@@ -92,3 +92,401 @@ function smath($a, $b, $math='+', $num=4, $func='floor')
 
     return handleMoney($re, $num, $func);
 }
+
+/* 正则匹配 */
+function regMobile($mobile)
+{
+    return preg_match('/1[\d]{10}/', $mobile);
+}
+function regEmail($email)
+{
+    return preg_match('/\w[\w\-\.]+\@\w[\w\-]+\.\w[\w\-]+/Ui', $email);
+}
+
+/* 表单验证 */
+function formHash($new=0)
+{
+    if ($new == 1)
+    {
+        $_SESSION['formhash'] = srandsalt(4);
+    }
+
+    return $_SESSION['formhash'];
+}
+function formCheck($formhash)
+{
+    if ($formhash == formHash())
+    {
+        unset($_SESSION['formhash']);
+        return true;
+    }
+    return false;
+}
+
+function gbk2utf8($str)
+{
+    return sencode($str, 'gbk', 'utf-8');
+}
+
+function utf82gbk($str)
+{
+    return sencode($str, 'utf-8', 'gbk');
+}
+
+// 转换编码
+function sencode($str, $from, $to)
+{
+    $str = is_array($str) ? $str : array($str);
+
+    foreach ($str as $key => $value)
+    {
+        if (is_array($value))
+        {
+            $str[$key] = sencode($value, $from, $to);
+            continue;
+        }
+        $str[$key] = mb_convert_encoding($value, $to, $from);
+    }
+
+    return $str;
+}
+
+function getClientIP()
+{
+    if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR')
+        && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $ip = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']
+        && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+}
+
+/* SQL ADDSLASHES 参数可以是字符串或者数组 */
+function saddslashes($string)
+{
+    if (is_array($string))
+    {
+        foreach($string as $key => $val)
+        {
+            $string[$key] = saddslashes($val);
+        }
+    }
+    else
+    {
+        $string = addslashes($string);
+    }
+
+    return $string;
+}
+
+function conn($db='db')
+{
+    global $_SGLOBAL, $_SC;
+    $_SGLOBAL[$db] = mysqli_connect($_SC[$db.'_host'], $_SC[$db.'_user'], $_SC[$db.'_upwd']);
+    mysqli_set_charset($_SC['charset'], 'utf8');
+}
+
+/*
+    返回随机字符串或者数组
+    Params:
+        $count: 返回个数
+        $addchars: 增加其他随机字符
+        $arrayout: 是否数组输出
+        $unique: 是否允许重复
+*/
+function srandsalt($count=6, $addchars='', $arrayout=0, $unique=0)
+{
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.$addchars;
+    if ($unique)
+    {
+        $tmp = range(0, strlen($chars)-1);
+        shuffle($tmp);
+        $keys = array_slice($tmp, 0, $count);
+        unset($tmp);
+    }
+    else
+    {
+        $keys = array_rand(range(0, strlen($chars)-1), $count);
+    }
+
+    $buff = array();
+    foreach($keys as $value)
+    {
+        $buff[] = $chars{$value};
+    }
+
+    if ($arrayout)
+    {
+        return $buff;
+    }
+    else
+    {
+        return implode('', $buff);
+    }
+}
+
+/* 生成缩略图 */
+function imageResize($srcFile, $toW, $toH, $toFile="")
+{
+    $toFile = $toFile ? $toFile : $srcFile;
+    $info = "";
+    $data = GetImageSize($srcFile, $info);
+
+    switch ($data[2])
+    {
+        case 1:
+            if(!function_exists("imagecreatefromgif"))
+            {
+                echo 'curl_imagecreatefromgif';
+                exit();
+            }
+
+            $im = ImageCreateFromGIF($srcFile);
+            break;
+        case 2:
+            if(!function_exists("imagecreatefromjpeg"))
+            {
+                echo 'curl_imagecreatefromjpeg';
+                exit();
+            }
+
+            $im = ImageCreateFromJpeg($srcFile);
+            break;
+        case 3:
+            $im = ImageCreateFromPNG($srcFile);
+            break;
+    }
+
+    $srcW=ImageSX($im);
+    $srcH=ImageSY($im);
+    $toWH=$toW/$toH;   // 缩放后
+    $srcWH=$srcW/$srcH;
+
+    if($toWH<=$srcWH)  // 比率
+    {
+        $ftoW=$toW;
+        $ftoH=$ftoW*($srcH/$srcW);
+    }
+    else
+    {
+        $ftoH=$toH;
+        $ftoW=$ftoH*($srcW/$srcH);
+    }
+
+    /*if( $srcW>$toW || $srcH>$toH )
+    {*/
+    if(function_exists("imagecreatetruecolor"))
+    {
+        @$ni = ImageCreateTrueColor($ftoW,$ftoH);
+        if($ni)
+        {
+            ImageCopyResampled($ni,$im,0,0,0,0,$ftoW,$ftoH,$srcW,$srcH);
+        }
+        else
+        {
+            $ni=ImageCreate($ftoW,$ftoH);
+            ImageCopyResized($ni,$im,0,0,0,0,$ftoW,$ftoH,$srcW,$srcH);
+        }
+    }
+    else
+    {
+        $ni=ImageCreate($ftoW,$ftoH);
+        ImageCopyResized($ni,$im,0,0,0,0,$ftoW,$ftoH,$srcW,$srcH);
+    }
+
+    if(function_exists('imagejpeg'))
+    {
+        ImageJpeg($ni,$toFile);
+        return true;
+    }
+    else
+    {
+        ImagePNG($ni,$toFile);
+        return true;
+    }
+    ImageDestroy($ni);
+
+    /*}*/
+
+    ImageDestroy($im);
+    return false;
+}
+
+/**
+ * 加密和解密函数
+ *
+ * <code>
+ * // 加密用户ID和用户名
+ * $auth = authcode("{$uid}\t{$username}", 'ENCODE');
+ * // 解密用户ID和用户名
+ * list($uid, $username) = explode("\t", authcode($auth, 'DECODE'));
+ * </code>
+ *
+ * @access public
+ * @param  string  $string    需要加密或解密的字符串
+ * @param  string  $operation 默认是DECODE即解密 ENCODE是加密
+ * @param  string  $key       加密或解密的密钥 参数为空的情况下取全局配置encryption_key
+ * @param  integer $expiry    加密的有效期(秒)0是永久有效 注意这个参数不需要传时间戳
+ * @return string
+ */
+function authcode($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+    global $_SC;
+
+    if ($operation == 'DECODE')
+    {
+        $string = str_replace('_', '+', $string);
+    }
+
+    $ckey_length = 4;
+    $key = md5($key != '' ? $key : $_SC['encryption_key']);
+    $keya = md5(substr($key, 0, 16));
+    $keyb = md5(substr($key, 16, 16));
+    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length) : substr(md5(microtime()), -$ckey_length)) : '';
+
+    $cryptkey = $keya . md5($keya . $keyc);
+    $key_length = strlen($cryptkey);
+
+    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string . $keyb), 0, 16) . $string;
+    $string_length = strlen($string);
+
+    $result = '';
+    $box = range(0, 255);
+
+    $rndkey = array();
+    for ($i = 0; $i <= 255; $i++) {
+        $rndkey[$i] = ord($cryptkey[$i % $key_length]);
+    }
+
+    for ($j = $i = 0; $i < 256; $i++) {
+        $j = ($j + $box[$i] + $rndkey[$i]) % 256;
+        $tmp = $box[$i];
+        $box[$i] = $box[$j];
+        $box[$j] = $tmp;
+    }
+
+    for ($a = $j = $i = 0; $i < $string_length; $i++) {
+        $a = ($a + 1) % 256;
+        $j = ($j + $box[$a]) % 256;
+        $tmp = $box[$a];
+        $box[$a] = $box[$j];
+        $box[$j] = $tmp;
+        $result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+    }
+
+    if ($operation == 'DECODE') {
+        if ((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26) . $keyb), 0, 16)) {
+            return substr($result, 26);
+        } else {
+            return '';
+        }
+    } else {
+        $result = str_replace('=', '', base64_encode($result));
+        //$result = str_replace('+', '_', $result);
+        return $keyc . $result;
+    }
+}
+
+/**
+ * 二维数组排序 按照指定的key 对数组进行排序
+ *
+ * @param array $arr 将要排序的数组
+ * @param string $keys 指定排序的key
+ * @param string $type 排序类型 asc | desc
+ *
+ * @return array
+ */
+function arraySort($arr, $keys, $type = 'asc')
+{
+    $keysvalue = $new_array = array();
+
+    foreach ($arr as $k => $v)
+    {
+        $keysvalue[$k] = $v[$keys];
+    }
+
+    $type == 'asc' ? asort($keysvalue) : arsort($keysvalue);
+    reset($keysvalue);
+
+    foreach ($keysvalue as $k => $v)
+    {
+        $new_array[$k] = $arr[$k];
+    }
+    return $new_array;
+}
+
+/**
+ * curl请求
+ */
+function http($url, $method = 'GET', $postfields = null, $headers = array(), $https = false)
+{
+    $ci = curl_init();
+
+    /* Curl settings */
+    curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+    curl_setopt($ci, CURLOPT_USERAGENT, 'zhzcl');
+    curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ci, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+
+    switch ($method)
+    {
+        case 'POST':
+            curl_setopt($ci, CURLOPT_POST, true);
+            if (!empty($postfields))
+            {
+                curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
+            }
+
+            break;
+    }
+
+    if ($https)
+    {
+        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ci, CURLOPT_SSL_VERIFYHOST, true);
+    }
+
+    curl_setopt($ci, CURLOPT_URL, $url);
+    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ci, CURLINFO_HEADER_OUT, true);
+
+    $response = curl_exec($ci);
+    $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+    curl_close($ci);
+
+    if ($http_code != 200)
+    {
+        return array($http_code, 'Curl error: ' . curl_error($ci));
+    }
+
+    return array($http_code, $response);
+}
+
+/**
+ * 权重算法
+*/
+function countWeight($data)
+{
+    $weight = 0;
+    $temp = array();
+    foreach($data as $v)
+    {
+        $weight += $v['weight'];
+        for($i=0; $i<$v['weight']; $i++)
+        {
+            $temp[] = $v;//放大数组
+        }
+    }
+
+    $int = mt_rand(0, $weight-1);//获取一个随机数
+    $result = $temp[$int];
+    return $result;
+}
+
