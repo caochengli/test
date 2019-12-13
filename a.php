34ef7905ff982d9841cd343c011539f571bd0625
ccl <912614339@qq.com>
2019-12-13 15:09:15 +0800

    测试sourcetree 提交

--- a.php
+++ a.php
@@ -1,6 +1,30 @@
 <?php
 include 'common_function.php';
 
+echo strlen('https://mp.weixin.qq.com/s/vCZgkQ92wBNcUBIFj3IvVg');
+exit;
+
+echo md5('anhuixt#*95net');
+exit;
+
+$a = floatval('0.0000');
+$b = empty($a);
+var_dump($b);
+exit;
+if (empty($a)) {
+    echo '11';
+} else {
+    echo '22';
+}
+
+echo substr(md5('joystar@95net'), 5, 20);exit;
+echo md5('czzccn@95net');exit;
+
+
+echo md5('jiayi@95net');exit;
+
+echo md5('zuche0551@95net');exit;
+
 //$proArr = array(0 => 1);
 //$proArr = array( 1 => 0.01, 2 => 0.05, 3 => 0.00090, 7 => 50);
 //$proArr = array(1 => 0.01, 2 => 0.05, 3 => 0.00090);
